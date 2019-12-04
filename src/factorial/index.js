96264a38cc6dad78cfdcbbaeba8431bb124d29f1
Itamar Kestenbaum <itamar.kestenbaum@lemonade.com>
2019-12-04 18:52:33 +0200

    adding

--- src/factorial/index.js
+++ src/factorial/index.js
@@ -1,12 +1,5 @@
-export function factorialIterative(num) {
-    let answer = 1;
-    for(var i = 2; i <= num; i++){
-        answer *= i;
-    }
-    return answer;
+export function factorialIterative() {
 }
 
-export function factorialRecursive(num) {
-    if(num < 2) return num;
-    return num *= factorialRecursive(num-1);
+export function factorialRecursive() {
 }
--- src/say/index.js
+++ src/say/index.js
@@ -1,16 +1,8 @@
-export function say(str) {
-    return str + say(str);
+export function say() {
 }
 
-export function sayIterative(str, n) {
-    const val = [];
-    for(let i = 0; val.length < n; i++){
-        val.push(str);
-    }
-    return val.join(' ');
+export function sayIterative() {
 }
 
-export function sayRecursive(str, n) {
-    if(n === 1) return str;
-    return `${str} ${sayRecursive(str, n-1)}`;
+export function sayRecursive() {
 }
